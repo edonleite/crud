7435970d849d1cbfe61d2f58b351a8dfc7e4f3f7
edonleite <edonleite@gmail.com>
2024-12-22 23:23:55 -0300

    retirou o menu cadastro

--- index.php
+++ index.php
@@ -14,7 +14,7 @@
   <header>
     <nav class="navbar navbar-expand-lg navbar-light bg-light">
       <div class="container">
-        <a class="navbar-brand" href="#" lang="pt-BR">Cadastro</a>
+      <!--  <a class="navbar-brand" href="#" lang="pt-BR">Cadastro</a> -->
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" 
                 aria-controls="navbarNav" aria-expanded="false" aria-label="Alternar navegação">
           <span class="navbar-toggler-icon"></span>
